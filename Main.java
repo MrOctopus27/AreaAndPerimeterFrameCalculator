b9bb2f9ac276b2e18dcdacfa39bb5600c8a251e6
Kevin Alvarado <110209254+MrOctopus27@users.noreply.github.com>
2023-04-23 21:33:21 -0500

    Create Main.java

---
+++ Main.java
@@ -0,0 +1,23 @@
+/*
+ * Author: Kevin Alvarado	
+ * Date: 11/15/2022
+ * 
+ * Description: This program find the area and perimeter of a recatngle based on the length and the width of the object.
+ */
+
+package murach;
+
+import java.util.ArrayList;
+
+
+import murach.ui.AreaAndPerimeterFrame;
+
+public class Main {
+
+    public static void main(String[] args) {
+        new AreaAndPerimeterFrame();
+    }
+    
+}
+
+
